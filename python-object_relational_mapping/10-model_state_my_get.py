83e7cca4021bf88ffee52d1d99203a2176ee0874
honnete-1 <nishimweh777@gmail.com>
2025-11-30 23:11:09 +0000

    c

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -5,35 +5,31 @@ from the database hbtn_0e_6_usa.
 """
 
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
 if __name__ == "__main__":
-    # 3 arguments: username, password, db name
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    # MySQL connection string
     engine = create_engine(
         f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
         pool_pre_ping=True
     )
 
-    # Create session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query: all states containing 'a'
-    states_with_a = session.query(State).filter(State.name.like('%a%')).all()
+    # Case-insensitive search for any 'a'
+    states_with_a = session.query(State)\
+        .filter(func.lower(State.name).like('%a%'))\
+        .all()
 
-    # Delete them
     for state in states_with_a:
         session.delete(state)
 
-    # Commit changes
     session.commit()
-
     session.close()
