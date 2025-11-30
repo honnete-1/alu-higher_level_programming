967a8ee41bedb91277830f205f17495c8fdd7844
honnete-1 <nishimweh777@gmail.com>
2025-11-30 22:42:22 +0000

    c

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""
-Fetches https://alu-intranet.hbtn.io/status using urllib
-"""
-
+"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
-url = "https://alu-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
 
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
